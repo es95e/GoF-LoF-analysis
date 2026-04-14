e03e8e0882d4abaa40d2abaf59f265d81fac21f1
es95e <sel.ema@hotmail.it>
2026-04-14 17:08:22 +0200

    GoF_LoF_statistical_validation.py

--- GoF_LoF_statistical_validation.py
+++ GoF_LoF_statistical_validation.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2026 Emanuele Selleri - University of Parma
+# Licensed under the MIT License
+#!/usr/bin/env python3
 import pandas as pd
 import numpy as np
 from sklearn.metrics import roc_auc_score
